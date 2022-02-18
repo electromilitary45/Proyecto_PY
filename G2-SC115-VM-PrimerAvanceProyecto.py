d3bd26a0cb25a5c7cb7195c57544d252c549f48d
Sebastian Villalobos <derekl2045@hotmail.com>
2022-02-18 13:40:44 -0600

    Update G2-SC115-VM-PrimerAvanceProyecto.py

--- G2-SC115-VM-PrimerAvanceProyecto.py
+++ G2-SC115-VM-PrimerAvanceProyecto.py
@@ -1,1 +1,29 @@
-nom=input("nombre")
+#---variables---
+user="admin"
+passw="admin"
+
+#----/LOGIN---/
+userIn=input("Introduzca su usuario: ")
+passwIn=input("Introduzca su contraseña: ")
+
+if userIn== user and passwIn== passw:
+    tipoReg=int(input("Que quiere registrar? 1.Visitantes 2.Agenda de visita"))
+    if tipoReg==1:
+        print("----------------\nCompleto lo solicitado!")
+        nomCom=input("Nombre completo: ")
+        id=int(input("Cédula: "))
+        phoneNum=int(input("Número de teléfono: "))
+        email=input("Correo electrónico: ")
+        address=input("Direccion: ")
+        nacionalidad=int(input("Digite si usted es 'Nacional' o 'Extrangero': "))
+        ageG=int(input("Digite si es 1.Niño, 2.Adulto o 3.Adulto mayor: "))
+        
+        print("Registro Exitoso para el usuario",nomCom)
+    else:
+        print("¡Esta opción no está disponible!")
+else:
+    print("Usuario no encontrado")
+
+
+
+
